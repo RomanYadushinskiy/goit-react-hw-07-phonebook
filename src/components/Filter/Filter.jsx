32da6d468c8a18561ce37b621811baf46622def7
rdush <rdush824@gmail.com>
2023-02-10 00:10:04 +0200

    hw done

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,35 +1,3 @@
-// import PropTypes from 'prop-types';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { setFilter, getFilter } from '../../redux/slice';
-
-// import './Filter.module.css';
-
-// export const Filter = () => {
-//   const dispatch = useDispatch();
-//   const filter = useSelector(getFilter);
-
-//   const getChangeFilter = e => {
-//     dispatch(setFilter(e.target.value));
-//   };
-
-//   return (
-//     <label>
-//       <input
-//         type="text"
-//         name="filter"
-//         value={filter}
-//         onChange={getChangeFilter}
-//         placeholder="Find contacts by name"
-//       />
-//     </label>
-//   );
-// };
-
-// Filter.propTypes = {
-//   filter: PropTypes.string,
-//   getChangeFilter: PropTypes.func,
-// };
-
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter, getFilter } from '../../redux/filterSlice';
 
